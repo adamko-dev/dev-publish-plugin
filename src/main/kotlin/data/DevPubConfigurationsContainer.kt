716c776e7e8d3f40a6ab2187f0da4cfda373f4fe
Adam <897017+aSemy@users.noreply.github.com>
2024-07-12 14:26:24 +0200

    Update Configurations
    - Fix extendsFrom.
    - Add Category attribute (apparently it's better than Usage? Although there's no explanation for this https://github.com/gradle/gradle/issues/27991#issuecomment-1952055976)
    - Experiment with filtering via artifact view... (but it doesn't work because Gradle sucks https://github.com/gradle/gradle/issues/27991)
    - Create Configurations rather than register them, because Configurations get eagerly created anyway.

--- src/main/kotlin/data/DevPubConfigurationsContainer.kt
+++ src/main/kotlin/data/DevPubConfigurationsContainer.kt
@@ -8,19 +8,23 @@ import dev.adamko.gradle.dev_publish.data.DevPubAttributes.Companion.DevPublishT
 import dev.adamko.gradle.dev_publish.internal.DevPublishInternalApi
 import dev.adamko.gradle.dev_publish.utils.consumable
 import dev.adamko.gradle.dev_publish.utils.declarable
+import dev.adamko.gradle.dev_publish.utils.get
 import dev.adamko.gradle.dev_publish.utils.resolvable
-import org.gradle.api.NamedDomainObjectProvider
+import java.io.File
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.artifacts.result.ResolvedArtifactResult
+import org.gradle.api.attributes.Category.CATEGORY_ATTRIBUTE
 import org.gradle.api.attributes.Usage.USAGE_ATTRIBUTE
+import org.gradle.api.provider.Provider
 
 /**
  * Utility class that contains all [Configuration]s used by [dev.adamko.gradle.dev_publish.DevPublishPlugin].
  */
 @DevPublishInternalApi
 class DevPubConfigurationsContainer(
-  devPubAttributes: DevPubAttributes,
+  private val devPubAttributes: DevPubAttributes,
   dependencies: DependencyHandler,
   configurations: ConfigurationContainer,
 ) {
@@ -30,41 +34,89 @@ class DevPubConfigurationsContainer(
     dependencies.attributesSchema.attribute(DevPublishTypeAttribute)
   }
 
-  private val devPublicationApiDependencies: NamedDomainObjectProvider<Configuration> =
-    configurations.register(DevPublishPlugin.DEV_PUB__PUBLICATION_API_DEPENDENCIES) {
+  private val devPublicationApiDependencies: Configuration =
+    configurations.create(DevPublishPlugin.DEV_PUB__PUBLICATION_API_DEPENDENCIES) {
       description =
-        "Declare dependencies on test Maven Publications, that will be propagated to consumers of this subproject"
+        "Declare dependencies on test Maven Publications." +
+            "The publications will also be shared with consumers of this subproject."
       declarable()
     }
 
-  private val devPublicationDependencies: NamedDomainObjectProvider<Configuration> =
-    configurations.register(DEV_PUB__PUBLICATION_DEPENDENCIES) {
-      description = "Declare dependencies on test Maven Publications"
+  private val devPublicationDependencies: Configuration =
+    configurations.create(DEV_PUB__PUBLICATION_DEPENDENCIES) {
+      description = "Declare dependencies on test Maven Publications."
       declarable()
-      extendsFrom(devPublicationApiDependencies.get())
     }
 
-  val testMavenPublicationResolver: NamedDomainObjectProvider<Configuration> =
-    configurations.register(DEV_PUB__PUBLICATION_INCOMING) {
-      description = "Resolve test Maven Publications"
+  val testMavenPublicationResolver: Configuration =
+    configurations.create(DEV_PUB__PUBLICATION_INCOMING) {
+      description = "Resolve test Maven Publications."
       resolvable()
       attributes {
         attribute(USAGE_ATTRIBUTE, devPubAttributes.devPublishUsage)
+        attribute(CATEGORY_ATTRIBUTE, devPubAttributes.devPublishCategory)
         attribute(DevPublishTypeAttribute, devPubAttributes.mavenRepositoryType)
       }
-      extendsFrom(devPublicationDependencies.get())
+      extendsFrom(devPublicationApiDependencies)
+      extendsFrom(devPublicationDependencies)
     }
 
-  val testMavenPublicationApiElements: NamedDomainObjectProvider<Configuration> =
-    configurations.register(DEV_PUB__PUBLICATION_OUTGOING) {
-      description = "Provide test Maven Publications"
+  val testMavenPublicationApiElements: Configuration =
+    configurations.create(DEV_PUB__PUBLICATION_OUTGOING) {
+      description = "Provide test Maven Publications."
       consumable()
       attributes {
         attribute(USAGE_ATTRIBUTE, devPubAttributes.devPublishUsage)
+        attribute(CATEGORY_ATTRIBUTE, devPubAttributes.devPublishCategory)
         attribute(DevPublishTypeAttribute, devPubAttributes.mavenRepositoryType)
       }
-      extendsFrom(devPublicationApiDependencies.get())
+      extendsFrom(devPublicationApiDependencies)
     }
+
+  fun resolvedDevRepos1(): Provider<List<File>> {
+    return testMavenPublicationResolver
+      .incoming
+      .artifactView {
+        attributes {
+          attribute(USAGE_ATTRIBUTE, devPubAttributes.devPublishUsage)
+          attribute(CATEGORY_ATTRIBUTE, devPubAttributes.devPublishCategory)
+          attribute(DevPublishTypeAttribute, devPubAttributes.mavenRepositoryType)
+        }
+        lenient(true)
+        @Suppress("UnstableApiUsage")
+        withVariantReselection()
+      }
+      .artifacts
+      .resolvedArtifacts
+      .map { artifacts ->
+        artifacts
+          .filter {
+            /**/ it.variant.attributes[USAGE_ATTRIBUTE] == devPubAttributes.devPublishUsage
+              && it.variant.attributes[CATEGORY_ATTRIBUTE] == devPubAttributes.devPublishUsage
+              && it.variant.attributes[DevPublishTypeAttribute] == devPubAttributes.mavenRepositoryType
+          }
+          .map(ResolvedArtifactResult::getFile)
+      }
+  }
+
+//  fun resolvedDevRepos2() {
+//    testMavenPublicationResolver
+//      .incoming
+//      .resolutionResult
+//      .rootComponent
+//      .map { component ->
+//        component.variants
+//          .filter {
+//            /**/ it.attributes[USAGE_ATTRIBUTE] == devPubAttributes.devPublishUsage
+//              && it.attributes[CATEGORY_ATTRIBUTE] == devPubAttributes.devPublishUsage
+//              && it.attributes[DevPublishTypeAttribute] == devPubAttributes.mavenRepositoryType
+//          }
+//          .flatMap { component.getDependenciesForVariant(it) }
+//          .map { it }
+//
+//        component
+//      }
+//  }
 
   @DevPublishInternalApi
   companion object
