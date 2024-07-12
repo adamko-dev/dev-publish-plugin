3b3b35ef45dbf69e7a698a829e2926bbaccdf889
Adam <897017+aSemy@users.noreply.github.com>
2024-07-12 12:16:34 +0200

    make testFixture util public

--- src/testFixtures/kotlin/stringUtils.kt
+++ src/testFixtures/kotlin/stringUtils.kt
@@ -2,5 +2,5 @@ package dev.adamko.gradle.dev_publish.test_utils
 
 
 /** Replace characters that don't match [isLetterOrDigit] with [replacement]. */
-internal fun String.replaceNonAlphaNumeric(replacement: String = "-"): String =
+fun String.replaceNonAlphaNumeric(replacement: String = "-"): String =
   map { if (it.isLetterOrDigit()) it else replacement }.joinToString("")
