0be140649e05f117a70a9e90049c1220c1c7a29d
Adam <897017+aSemy@users.noreply.github.com>
2024-07-13 14:24:50 +0200

    update file checksums generator (#26)
    - Hide checksum type, and rename function so 'md5' isn't implied.
    - Use SHA-256 (md5 vs sha256 doesn't really matter, but why not)
    - Base64 encode (smaller log output)

---
+++ src/main/kotlin/utils/checksum.kt
@@ -0,0 +1,23 @@
+package dev.adamko.gradle.dev_publish.utils
+
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream.nullOutputStream
+import java.security.DigestOutputStream
+import java.security.MessageDigest
+import java.util.*
+
+internal fun File.checksum(): String =
+  if (exists()) inputStream().checksum() else "missing"
+
+private fun InputStream.checksum(): String {
+  val messageDigester = MessageDigest.getInstance("SHA-256")
+
+  buffered().use { input ->
+    DigestOutputStream(nullOutputStream(), messageDigester).use { digestStream ->
+      input.copyTo(digestStream)
+    }
+  }
+
+  return Base64.getEncoder().encodeToString(messageDigester.digest())
+}
