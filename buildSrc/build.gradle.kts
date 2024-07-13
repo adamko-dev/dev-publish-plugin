98ade5011b6443a04d385ae21581b6355a06710e
Adam <897017+aSemy@users.noreply.github.com>
2024-07-13 14:47:09 +0200

    Add reproducible archive config to buildSrc (#31)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,3 +10,9 @@ dependencies {
   implementation(libs.gradlePlugin.pluginPublishPlugin)
   implementation(libs.gradlePlugin.bcvMu)
 }
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+  // https://docs.gradle.org/current/userguide/working_with_files.html#sec:reproducible_archives
+  isPreserveFileTimestamps = false
+  isReproducibleFileOrder = true
+}
