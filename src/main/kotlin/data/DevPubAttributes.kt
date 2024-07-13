d56bd870bc1192c62a4cdeba6b710857e0e65734
Adam <897017+aSemy@users.noreply.github.com>
2024-07-13 15:02:49 +0200

    Update Configuration Attributes
    - Remote typesafe attribute - they are bugged https://github.com/gradle/gradle/issues/28695
    - Add Category attribute (apparently it's better than Usage? Although there's no explanation for this gradle/gradle#27991 (comment))

--- src/main/kotlin/data/DevPubAttributes.kt
+++ src/main/kotlin/data/DevPubAttributes.kt
@@ -2,9 +2,9 @@ package dev.adamko.gradle.dev_publish.data
 
 import dev.adamko.gradle.dev_publish.internal.DevPublishInternalApi
 import dev.adamko.gradle.dev_publish.utils.Attribute
-import org.gradle.api.Named
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.attributes.Attribute
+import org.gradle.api.attributes.Category
 import org.gradle.api.attributes.Usage
 import org.gradle.api.model.ObjectFactory
 import org.gradle.kotlin.dsl.named
@@ -18,17 +18,17 @@ import org.gradle.kotlin.dsl.named
 class DevPubAttributes(
   objects: ObjectFactory,
 ) {
-  @DevPublishInternalApi
-  interface DevPublishType : Named
-
   /** Indicates a [Configuration] contains a Maven Repository */
   val devPublishUsage: Usage = objects.named("dev-publish")
 
-  val mavenRepositoryType: DevPublishType = objects.named("maven-repository")
+  /** Indicates a [Configuration] contains a Maven Repository */
+  val devPublishCategory: Category = objects.named("dev-publish")
+
+  val mavenRepositoryType = "maven-repository"
 
   @DevPublishInternalApi
   companion object {
-    val DevPublishTypeAttribute: Attribute<DevPublishType> =
+    val DevPublishTypeAttribute: Attribute<String> =
       Attribute("dev.adamko.gradle.dev_publish.type")
   }
 }
